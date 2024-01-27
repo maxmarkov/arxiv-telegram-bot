37e3668723b5582cf4238227f2807e4a0b32cc35
maxmarkov <maxmarkov@gmail.com>
2024-01-27 21:09:32 +0100

    refactor arxiv bot

--- bot/arxiv_api.py
+++ bot/arxiv_api.py
@@ -1,4 +1,6 @@
 import re
+import json
+import time
 import html
 import logging
 import urllib
@@ -7,140 +9,258 @@ import feedparser
 
 from datetime import datetime
 
-def get_current_date_formatted() -> str:
-  """ Get the current date and format it as a string in the "yymm" format.
+class ArxivFetcher:
+    """ A class to fetch articles metadata using arXiv API. """
+    def __init__(self, category: str='q-fin.PM', date: str=""):
+      """ Initialize the ArxivFetcher with a default category and date.
+      Args:
+          category (str): The category of articles to fetch from the arXiv API. 
+          date (str): The date to fetch articles from in the format 'yymm'. Defaults to the current date.
+      """
+      self.category = category
+      self.date = date if date else self.get_current_date_formatted()
+      self.validate_date_format(self.date)
+      self.entries = {}
+      self.ids = []
 
-  Returns:
-      str: The current date formatted as "yymm", where 'yy' is the last two digits of the year
-          and 'mm' is the month.
-  """
-  current_date = datetime.now()
-  formatted_date = current_date.strftime("%y%m")
-  return formatted_date
+    def get_current_date_formatted(self) -> str:
+      """ Get the current date and format it as a string in the "yymm" format.
+      Returns:
+          str: The current date formatted as "yymm", where 'yy' is the last two digits of the year
+              and 'mm' is the month.
+      """
+      current_date = datetime.now()
+      formatted_date = current_date.strftime("%y%m")
+      return formatted_date
 
-def fetch_arxiv_updates(cat: str = 'q-fin.PM', date="") -> bytes:
-  """ Fetches the list of articles in a specified category from the arXiv API using the urllib.request module.
+    def validate_date_format(self, date: str) -> None:
+      """ Validate the date format. """
+      if not re.match(r"\d{4}", date):
+        error_message = "Date must be in 'yymm' format or empty"
+        logging.error(error_message)
+        raise ValueError(error_message)
 
-  This function constructs a URL for the arXiv API request based on the given category and the current date, 
-  formatted appropriately. It then makes a request to the arXiv API and returns the response in bytes format.
+    def fetch_updates(self) -> bytes:
+      """ Fetches the list of articles in a specified category from the arXiv API using the urllib.request module.
 
-  Args: 
-    cat (str): The category of articles to fetch from the arXiv API. 
-              Default is 'q-fin.PM' (Quantitative Finance - Portfolio Management).
-              The category should be specified in the format 'subject.Class' 
-              as per arXiv's categorization.
-  Returns:
-    bytes: The raw response from the arXiv API, containing the list of articles in the specified category.
-          This is returned as a byte string.
+      This function constructs a URL for the arXiv API request based on the given category and the current date, 
+      formatted appropriately. It then makes a request to the arXiv API and returns the response in bytes format.
 
-  Example:
-    >>> response = fetch_arxiv_updates('cs.LG')
-    >>> print_response(response)
-  """
-  if date == "":
-    formatted_current_date = get_current_date_formatted()
-  else:
-    formatted_current_date = date
+      Returns:
+        bytes: The raw response from the arXiv API, containing the list of articles in the specified category.
+              This is returned as a byte string.
 
-  URL = f'http://export.arxiv.org//list/{cat}/{formatted_current_date}'
-  logging.info(URL)
-  response = urllib.request.urlopen(URL).read()
-  return response
+      Example:
+        >>> fetcher = ArxivFetcher(category='q-fin.PM')
+        >>> print_response(response)
+      """
+      url = f'http://export.arxiv.org//list/{self.category}/{self.date}'
+      logging.info(url)
+      response = urllib.request.urlopen(url).read()
+      return response
 
-def print_response(response: bytes) -> None:
-  """ Print a byte string response:
-  Args:
-    response (bytes): The raw response from the arXiv API, containing the list of articles in the specified category.
-                      This is returned as a byte string."""
-  response_str = response.decode('utf-8')
-  print(response_str)
+    @staticmethod
+    def print_response(response: bytes) -> None:
+      """ Print a byte string response.
+      Args:
+          response (bytes): The raw response from the arXiv API, containing the list of articles in the specified category.
+                  This is returned as a byte string.
+      """
+      response_str = response.decode('utf-8')
+      print(response_str)
 
-def parse_arxiv_response_re(response: bytes) -> dict:
-  """ Parses a response of a list query from the arXiv API with regular expressions and extracts relevant information
-  about each article.
+    def parse_arxiv_response_re(self, response: bytes) -> dict:
+      """ Parses a response of a list query from the arXiv API with regular expressions and extracts relevant information
+      about each article.
 
-  This function decodes the given byte-string response from the arXiv API, and uses regular expressions to extract 
-  various details like the total number of entries, titles, comments, authors, and paper IDs. Each article's details 
-  are compiled into a dictionary.
+      This function decodes the given byte-string response from the arXiv API, and uses regular expressions to extract 
+      various details like the total number of entries, titles, comments, authors, and paper IDs. Each article's details 
+      are compiled into a dictionary.
 
-  Args:
-    response (bytes): The raw response in bytes format obtained from the arXiv API.
-  Returns:
-    dict: A dictionary where each key is an arXiv paper ID and the value is another dictionary containing 
-        details of the paper such as title, authors, and a direct link to the PDF version.
-  """
+      Args:
+        response (bytes): The raw response in bytes format obtained from the arXiv API.
+      Returns:
+        dict: A dictionary where each key is an arXiv paper ID and the value is another dictionary containing 
+            details of the paper such as title, authors, and a direct link to the PDF version.
+      """
+      response_str = response.decode('utf-8')
 
-  results = {}
+      entries_pattern = r'total of (\d+) entries'
+      n_entries = re.search(entries_pattern, response_str).group(1) if re.search(entries_pattern, response_str) else None
+      logging.info(f"Total number of entries: {n_entries}")
 
-  # total number of papers
-  entries_pattern = r'total of (\d+) entries'
-  if re.search(entries_pattern, response.decode('utf-8')):
-    n_entries = re.search(entries_pattern, response.decode('utf-8')).group(1)
-  else:
-    n_entries = None
+      title_pattern = r'<span class="descriptor">Title:</span>([^\n]*)'
+      titles = [html.unescape(string).lstrip().replace("  ", " ") for string in re.findall(title_pattern, response_str)]
+      logging.info(f"Titles: {', '.join(titles)}")
 
-  # extract the title
-  title_pattern = r'<span class="descriptor">Title:</span>([^\n]*)'
-  titles = re.findall(title_pattern, response.decode('utf-8'))
-  titles = [html.unescape(string).lstrip().replace("  ", " ") for string in titles]
+      comment_pattern = r'<span class="descriptor">Comments:</span>([^\n]*)'
+      comments = [html.unescape(string).lstrip().replace("  ", " ") for string in re.findall(comment_pattern, response_str)]
 
-  # extract the comments
-  comment_pattern = r'<span class="descriptor">Comments:</span>([^\n]*)'
-  comments = re.findall(comment_pattern, response.decode('utf-8'))
-  comments = [html.unescape(string).lstrip().replace("  ", " ") for string in comments]
+      authors_pattern = r'<div class="list-authors">(.*?)</div>'
+      authors = [re.findall(r'<a href="[^"]*">(.*?)</a>', author) for author in re.findall(authors_pattern, response_str, re.DOTALL)]
 
-  # extract the list of authors
-  authors_pattern = r'<div class="list-authors">(.*?)</div>'
-  authors = re.findall(authors_pattern, response.decode('utf-8'), re.DOTALL)
-  author_names_pattern = r'<a href="[^"]*">(.*?)</a>'
-  authors = [re.findall(author_names_pattern, author) for author in authors]
+      links_pattern = r'<dt><a name="item(\d+)">\[\d+\]</a>(.*)'
+      links = re.findall(links_pattern, response_str)
 
-  # extract all paper ids
-  ids = []
-  links_pattern = r'<dt><a name="item(\d+)">\[\d+\]</a>(.*)'
-  links = re.findall(links_pattern, response.decode('utf-8'))
+      ids = []
+      arxiv_id_pattern = r'arXiv:(\d+\.\d+)'
+      for link in links:
+        arxiv_id = re.search(arxiv_id_pattern, link[1]).group(1) if re.search(arxiv_id_pattern, link[1]) else None
+        ids.append(arxiv_id)
 
-  arxiv_id_pattern = r'arXiv:(\d+\.\d+)'
-  for link in links:
-    if re.search(arxiv_id_pattern, link[1]):
-      arxiv_id = re.search(arxiv_id_pattern, link[1]).group(1)
-    else:
-      arxiv_id = None
-    ids.append(arxiv_id)
+      # gather all results together
+      results = {}
+      for i, id in enumerate(ids):
+        if id is not None:
+          results[id] = {'title': titles[i], 'authors': authors[i], 'pdf_export_link': f'http://export.arxiv.org/pdf/{id}'}
 
-  # gather all results together
-  for i, id in enumerate(ids):
-    results[id] = {'title': titles[i], 'authors': authors[i], 'pdf_export_link': f'http://export.arxiv.org/pdf/{id}'}
+      if results:
+        logging.info(f"{len(ids)} articles found: {ids}\n")
+        self.entries = results
+        self.ids = list(results.keys())
+      else:
+        logging.info("No articles found in the entries.\n")
 
-  return results
+      return results
 
-def query_arxiv(id: str) -> bytes:
-  """ Query arxiv API to get metadata of the article with certain ID
-  Args:
-    id (str): ID of the article to be queried
-  Returns:
-    bytes: The raw response from the arXiv API, containing the metadata of the article with the specified ID.
-          This is returned as a byte string.
-  """
-  query = f"http://export.arxiv.org/api/query?id_list={id}"
-  logging.info(f'Query: {query}')
+    def save_to_json(self, filepath: str) -> None:
+      """ Save the current state of the object to a JSON file.
+      Args:
+          filepath (str): The file path where the object's state will be saved.
+      Example:
+          >>> fetcher = ArxivFetcher(category='q-fin.PM')
+          >>> fetcher.save_to_json('fetcher.json')
+      """
+      with open(filepath, 'w') as file:
+          json.dump({
+              'category': self.category,
+              'date': self.date,
+              'entries': self.entries,
+              'ids': self.ids
+          }, file)
+      logging.info(f"ArxivFetcher state saved to {filepath}")
 
-  r = urllib.request.urlopen(query)
+    @classmethod
+    def load_from_json(cls, filepath: str):
+      """ Load the state of the object from a JSON file.     
+      Args:
+          filepath (str): The file path from where the object's state will be loaded.
+      Returns:
+          ArxivFetcher: An instance of ArxivFetcher with the state restored from the JSON file.
+      Example:
+          >>> fetcher = ArxivFetcher.load_from_json('fetcher.json')
+      """
+      with open(filepath, 'r') as file:
+          data = json.load(file)
+          fetcher = cls(data['category'], data['date'])
+          fetcher.entries = data.get('entries', {})
+          fetcher.ids = data.get('ids', [])
+      logging.info(f"ArxivFetcher state loaded from {filepath}")
+      return fetcher
 
-  feedparser.mixin._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
-  feedparser.mixin._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
-  feed = feedparser.parse(r)
-  return feed.entries
+    def split_list_into_groups(self, group_size=10):
+      """ Split a list into smaller groups.
+      Args:
+          group_size (int): The maximum size of each group.
+      Returns:
+          list of lists: The original list split into smaller groups.
+      """
+      if group_size > 10:
+        logging.warning("Group size is greater than 10. This may cause the arXiv API to return an error.")
+        
+      for i in range(0, len(self.ids), group_size):
+        yield self.ids[i:i + group_size]
 
-def query_arxiv_list(id_list: list) -> bytes:
-  """ Query arxiv API to get metadata of multiple articles from the list of IDs """
-  joined_ids = ','.join(id_list)
-  query = f"http://export.arxiv.org/api/query?id_list={joined_ids}"
-  logging.info(f'Query: {query}')
+    @staticmethod
+    def query_arxiv(ids) -> bytes:
+      """ Query arXiv API to get metadata of articles with certain IDs.
+      Args:
+        ids: A single ID as a string or a list of IDs to be queried.
+      Returns:
+        bytes: The raw response from the arXiv API, containing the metadata of the article(s) with the specified ID(s).
+             This is returned as a byte string.
+      """
+      if isinstance(ids, str):
+        ids = [ids]
 
-  r = urllib.request.urlopen(query)
+      joined_ids = ','.join(ids)
+      query = f"http://export.arxiv.org/api/query?id_list={joined_ids}"
+      logging.info(f'Query: {query}')
 
-  feedparser.mixin._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
-  feedparser.mixin._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
-  feed = feedparser.parse(r)
-  return feed.entries
+      r = urllib.request.urlopen(query)
+
+      feedparser.mixin._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
+      feedparser.mixin._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
+
+      feed = feedparser.parse(r)
+      return feed.entries
+
+    @staticmethod
+    def remove_after_keywords(text: str) -> str:
+      """ Remove everything after the keyword 'Keywords:'.
+      Args:
+        text: The text to be processed.
+      Returns:
+        str: The processed text.
+      """
+      keyword = "Keywords:"
+      index = text.find(keyword)
+      if index != -1:
+        return text[:index]
+      else:
+        return text 
+        
+    def fetch_metadata_groups(self) -> list:
+      """ Fetch metadata for groups of article IDs.
+      Returns:
+        list of dicts: Metadata of the articles with the specified IDs.
+      """
+      metadata = []
+      id_groups = self.split_list_into_groups()
+      for id_group in id_groups:
+        logging.info(f"Fetching metadata for IDs: {id_group}")
+        metadata += self.query_arxiv(id_group)
+        time.sleep(5)  # Wait for 5 seconds before the next API call
+      return metadata
+
+    def process_metadata_item(self, item, id_index):
+      """ Process a single metadata item.
+      Args:
+        item: The metadata item to process.
+        id_index: The index of the corresponding ID in self.ids.
+      Returns:
+        dict: Processed metadata item.
+      """
+      id = self.ids[id_index]
+      entry = self.entries[id]
+      return {
+          "id": id,
+          "abstract_link": item['id'],
+          "pdf_link": entry['pdf_export_link'],
+          "updated": item['updated'],
+          "published": item['published'],
+          "title": entry['title'],
+          "authors": ', '.join(entry['authors']),
+          "summary": self.remove_after_keywords(item['summary'].replace('\n', ' ').replace('  ', ' ')),
+          "arxiv_comment": "",
+          "arxiv_primary_category": item['arxiv_primary_category']['term']}
+
+    def fetch_metadata(self):
+      """ Fetch metadata for a list of article IDs.
+      Returns:
+        list of dicts: Metadata of the articles with the specified IDs.
+      Example:
+        >>> fetcher = ArxivFetcher(category='q-fin.PM')
+        >>> metadata = fetcher.fetch_metadata()
+      """
+      metadata = []
+      if not self.ids:
+        logging.warning("No article IDs found.")
+        return metadata
+
+      fetched_metadata = self.fetch_metadata_groups()
+      logging.info(f"Fetched metadata of {len(fetched_metadata)} articles.")
+
+      processed_data = [self.process_metadata_item(item, i) for i, item in enumerate(fetched_metadata)]
+      logging.info(f"Processed metadata of {len(processed_data)} articles.")
+      return processed_data
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import asyncio
 import logging 
@@ -7,8 +8,7 @@ import argparse
 from dotenv import load_dotenv
 from apscheduler.schedulers.background import BlockingScheduler, BackgroundScheduler
 
-from bot.arxiv_api import fetch_arxiv_updates, parse_arxiv_response_re, query_arxiv, query_arxiv_list
-from bot.utils import write_dict_to_json, read_json_to_dict, is_yesterday, remove_after_keywords, split_list_into_groups, remove_none_from_list
+from bot.arxiv_api import ArxivFetcher
 from bot.post import format_post_for_telegram
 from bot.telegram_bot import send_message_to_channel
 from bot.openai import summarize_abstract, convert_text_to_embedding
@@ -50,83 +50,50 @@ def run_scheduler(scheduler_type: str) -> None:
 
 def main():
     try:
-
+        fetcher = ArxivFetcher(category='q-fin.PM')
         logging.info("Fetching recent arXiv updates...")
-        response_list = fetch_arxiv_updates()
+        response = fetcher.fetch_updates()
         logging.info("Parsing the response...")
-        entries = parse_arxiv_response_re(response_list)
+        entries = fetcher.parse_arxiv_response_re(response)
 
-        ## --- testing --- ##
-        #write_dict_to_json(entries, 'data.json')
-        #entries = read_json_to_dict('data.json')
-        ## --- end of testing --- ##
+        #fetcher = ArxivFetcher.load_from_json('fetcher_state.json')
+        metadata = fetcher.fetch_metadata()
 
-        id_list = list(entries.keys())
-        id_list = remove_none_from_list(id_list)
-        if len(id_list) == 0:
-            raise Exception("No articles found in the id list.")
-        else:
-            logging.info(f"{len(id_list)} articles found: {id_list}\n")
-
-        # get secret environment variables
-        channel_id = os.getenv('CHANNEL_ID')
-        bot_token = os.getenv('BOT_TOKEN')
-        api_key = os.getenv('OPENAI_TOKEN')
-        db_password = os.getenv('POSTGRES_PASSWORD')
-        db_name = os.getenv('POSTGRES_DB')
-        db_table = os.getenv('POSTGRES_TABLE')
-        db_port = os.getenv('POSTGRES_PORT')
-
-        id_groups = split_list_into_groups(id_list)
-        metadata = []
-        for id_group in id_groups:
-            metadata += query_arxiv_list(id_group)
-        logging.info(f"{len(metadata)} articles found.")
-        time.sleep(5)
-
-        conn, cursor = connect_to_postgres(password=db_password, database=db_name, port=db_port)#, host='host.docker.internal')
+        conn, cursor = connect_to_postgres(password=os.getenv('POSTGRES_PASSWORD'),
+                                           database=os.getenv('POSTGRES_DB'),
+                                           port=os.getenv('POSTGRES_PORT'))#, host='host.docker.internal')
 
         if conn is None or cursor is None:
             raise Exception("Could not connect to the database.")
         
         # ## === embedding === ##
-        # records = retrieve_first_n_rows(n=2, cursor=cursor, table_name=db_table)
+        # records = retrieve_first_n_rows(n=2, cursor=cursor, table_name=os.getenv('POSTGRES_TABLE'))
         # summary = records[0][2].replace("\n", " ")
-        # embedding = convert_text_to_embedding(summary, api_key)
+        # embedding = convert_text_to_embedding(summary, os.getenv('OPENAI_TOKEN'))
         # ## === end of embedding === ##
 
-        ids_not_in_database = get_ids_not_in_database(id_list, cursor, db_table)
+        ids_not_in_database = get_ids_not_in_database(fetcher.ids, cursor, os.getenv('POSTGRES_TABLE'))
 
         if len(ids_not_in_database) > 0:
 
             logging.info(f"{len(ids_not_in_database)} articles to be submitted: {ids_not_in_database}")
 
             for i, item in enumerate(metadata):
+                print(metadata[i]['id'])
 
-                if id_list[i] in ids_not_in_database:
-           
-                    data = {"id": id_list[i],
-                            "abstract_link": item['id'],
-                            "pdf_link": entries[id_list[i]]['pdf_export_link'],
-                            "updated": item['updated'],
-                            "published": item['published'],
-                            "title": entries[id_list[i]]['title'],
-                            "authors": ', '.join(entries[id_list[i]]['authors']),
-                            "summary": remove_after_keywords(item['summary'].replace('\n', ' ').replace('  ', ' ')),
-                            "arxiv_comment": "",
-                            "arxiv_primary_category": item['arxiv_primary_category']['term'],}
+                if fetcher.ids[i] in ids_not_in_database:
                     
-                    logging.info(data)
-                    logging.info(f"Inserting {id_list[i]} into the database...")
-                    check_id_and_insert(cursor, conn, db_table, data)
+                    logging.info(metadata[i])
+                    logging.info(f"Inserting {fetcher.ids[i]} into the database...")
+                    check_id_and_insert(cursor, conn, os.getenv('POSTGRES_TABLE'), metadata[i])
                     logging.info("Data inserted successfully.\n")
 
-                    ai_summary = summarize_abstract(item['summary'], api_key)
+                    ai_summary = summarize_abstract(item['summary'], os.getenv('OPENAI_TOKEN'))
                     item['ai summary'] = ai_summary
 
                     message = format_post_for_telegram(item)
 
-                    asyncio.run(send_message_to_channel(bot_token, channel_id, message))
+                    asyncio.run(send_message_to_channel(os.getenv('BOT_TOKEN'), os.getenv('CHANNEL_ID'), message))
                     time.sleep(5)
         
         else:
